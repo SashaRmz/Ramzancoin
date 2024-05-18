f2e66f61e269503476c21f2d76e9d70c08be2b03
SashaRmz <alexandrtupoihach@gmail.com>
2024-05-18 06:00:55 +0300

    Update code.js

--- code/code.js
+++ code/code.js
@@ -1,38 +1,10 @@
-const TelegramBotAPI = require('node-telegram-bot-api');
-const express = require('express');
-const app = express();
-const port = 3000;
+const Telegraf = require('telegraf');
 
-// Replace with your bot token
-const botToken = '7134206125:AAFSJKeneH3vmfa9XZc29L-SHqAEskKCg5U';
+const bot = new Telegraf('7134206125:AAFSJKeneH3vmfa9XZc29L-SHqAEskKCg5U');
 
-const bot = new TelegramBotAPI(botToken);
-
-// Handle /start command
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  const webAppUrl = generateWebAppUrl(chatId); // Generate unique URL for web app
-  const replyMarkup = {
-    keyboard: [[{
-      text: 'Перейти к сайту', // Text for the button
-      web_app: { // Web App button object
-        url: webAppUrl // Replace with your web app URL
-      }
-    }]]
-  };
-
-  bot.sendMessage(chatId, 'Здравствуйте!', replyMarkup);
+bot.on('message', (ctx) => {
+  console.log(ctx.message.text); // Получить текст сообщения
+  ctx.reply('Привет! Я получил ваше сообщение.'); // Отправить ответ
 });
 
-// Implement web app logic here (e.g., routing, data handling)
-
-app.listen(port, () => {
-  console.log(Server listening on port ${port});
-});
-
-// Function to generate a unique URL for the web app (replace with your implementation)
-function generateWebAppUrl(chatId) {
-  // Use a service like Firebase Dynamic Links or a custom URL shortening service
-  // to generate a unique URL for the web app, specific to the chat ID
-  return https://sasharmz.github.io/Ramzancoin/ chat/${chatId}; // Replace with your actual URL
-}
+bot.launch();
