b2171849699dd274c0d1fdf85d9d294fc59faaf9
SashaRmz <alexandrtupoihach@gmail.com>
2024-05-18 06:08:26 +0300

    Update code.js

--- code/code.js
+++ code/code.js
@@ -8,3 +8,30 @@ bot.on('message', (ctx) => {
 });
 
 bot.launch();
+const { Telegraf, Markup } = require('telegraf');
+
+const bot = new Telegraf('7134206125:AAFSJKeneH3vmfa9XZc29L-SHqAEskKCg5U');
+
+// Обработчик команды /start
+bot.command('start', async (ctx) => {
+  // Отправляем приветственное сообщение
+  await ctx.sendMessage('Приветствую! ');
+
+  // Создаем кнопку для подключения к mini app
+  const miniAppButton = Markup.inlineKeyboardButton('Открыть mini app', {
+    url: 'https://sasharmz.github.io/Ramzancoin/', // Ссылка на ваш mini app
+  });
+
+  // Добавляем кнопку к клавиатуре
+  const keyboard = Markup.inlineKeyboard([
+    [miniAppButton],
+  ]);
+
+  // Отправляем сообщение с кнопкой
+  await ctx.sendMessage('Нажмите кнопку, чтобы открыть mini app:', {
+    reply_markup: keyboard,
+  });
+});
+
+// Запускаем бота
+bot.launch();
